083189995a887f404808463abac3b70ad535bbfe
Paulo <wvictor3011gmail.com>
2023-05-10 16:53:57 -0300

    este novo é o commit linux

--- app.js
+++ app.js
@@ -3,3 +3,4 @@ conmsole.olog("aplicação está se conetando ao banco de dados");
 //mais um console log de teste
 console.log("O paulo fez esse commit sem voce ver na aula!");
 console.log("commit linux");
+console.log("commit linux terminal");
