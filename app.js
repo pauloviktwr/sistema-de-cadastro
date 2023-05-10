41f87dc741d5a49d0e8b9d1a93cd3f88ba4e8065
Paulo <wvictor3011gmail.com>
2023-05-10 16:36:35 -0300

    commit terminal linux

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ comsole.log("Rodando o sistema de gerenciamento de jogos")
 conmsole.olog("aplicação está se conetando ao banco de dados");
 //mais um console log de teste
 console.log("O paulo fez esse commit sem voce ver na aula!");
+console.log("commit linux");
